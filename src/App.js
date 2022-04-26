6c70446d2a3b8be2077fbc7052c7795f9afe38ed
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-04-26 20:51:25 +0300

    feat: add creating new post

--- src/App.js
+++ src/App.js
@@ -2,28 +2,68 @@ import React, { useState, useReducer, useEffect } from "react";
 import styles from "./App.module.css";
 
 const initialState = {
-  items: [
-    {id: 1,
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
-  ], 
+  items: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."], 
 }
 
-function reducer(state, action) {
+function reducer(state = initialState, action) {
   switch (action.type) {
+    case "updateNotes": 
+      return { ...state, items: action.payload };
+      /*const newItem = {
+        id: Math.floor(new Date().getTime() + (Math.random() * 10)),
+        text: action.payload
+      }
+      console.log(state)*/
+      //state.items.push(newItem)
+      //console.log(state)*/
 
+    default:
+      return state;
   }
 }
 
 const App = () => {
   const [data, dispatch] = useReducer(reducer, initialState);
+  const [error, setError] = useState(false);
+  const [newItem, setNewItem] = React.useState("");
 
-  console.log(data)
+  /*const onClickAdd = (value) => {
+    if (value !== '') {
+      setError(false);
+      dispatch({ type: "new-note", payload: value });
+    } else {
+      setError(true);
+    }
+  };*/
+
+  const changeNewItem = (e) => {
+    setNewItem(e.target.value);
+  }
+
+  const dispatchNewItem = (e) => {
+    e.preventDefault();
+    if (newItem !== '') {
+      dispatch({
+        type: "updateNotes",
+        payload: [...data.items, newItem]
+      });
+      setNewItem("");
+    }
+  }
+
+
+  console.log(data.items)
+  console.log(error)
 
   return (
     <div>
+      <form onSubmit={dispatchNewItem}>
+        <input type="text" value={newItem} onChange={changeNewItem} />
+        <button>Create</button>
+      </form>
       <ul>
         {data.items.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <li>{item}</li>
         ))}
       </ul>
     </div>
