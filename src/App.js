6c8cd606722835c77a2d549319d3286af953ee92
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-04-26 20:59:12 +0300

    feat: add id to list

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React, { useState, useReducer, useEffect } from "react";
 import styles from "./App.module.css";
 
 const initialState = {
-  items: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."], 
+  items: [
+    {id: 1, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+  ], 
 }
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "updateNotes": 
+      console.log(action.payload)
       return { ...state, items: action.payload };
-      /*const newItem = {
-        id: Math.floor(new Date().getTime() + (Math.random() * 10)),
-        text: action.payload
-      }
-      console.log(state)*/
-      //state.items.push(newItem)
-      //console.log(state)*/
-
     default:
       return state;
   }
@@ -24,17 +19,7 @@ function reducer(state = initialState, action) {
 
 const App = () => {
   const [data, dispatch] = useReducer(reducer, initialState);
-  const [error, setError] = useState(false);
   const [newItem, setNewItem] = React.useState("");
-
-  /*const onClickAdd = (value) => {
-    if (value !== '') {
-      setError(false);
-      dispatch({ type: "new-note", payload: value });
-    } else {
-      setError(true);
-    }
-  };*/
 
   const changeNewItem = (e) => {
     setNewItem(e.target.value);
@@ -42,28 +27,30 @@ const App = () => {
 
   const dispatchNewItem = (e) => {
     e.preventDefault();
+    const obj = {
+      id: Math.floor(new Date().getTime() + (Math.random() * 10)),
+      text: newItem
+    }
     if (newItem !== '') {
       dispatch({
         type: "updateNotes",
-        payload: [...data.items, newItem]
+        payload: [...data.items, obj]
       });
       setNewItem("");
     }
   }
 
-
   console.log(data.items)
-  console.log(error)
 
   return (
     <div>
       <form onSubmit={dispatchNewItem}>
         <input type="text" value={newItem} onChange={changeNewItem} />
-        <button>Create</button>
+        <button>Добавить заметку</button>
       </form>
       <ul>
         {data.items.map((item) => (
-          <li>{item}</li>
+          <li key={item.id}>{item.text}</li>
         ))}
       </ul>
     </div>
