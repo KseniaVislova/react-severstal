909678d47ddd29f359998284523dbf238c7b0cca
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-04-26 23:10:50 +0300

    feat: add local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useReducer, useEffect } from "react";
 import styles from "./App.module.css";
 
-const initialState = {
-  items: [
-    {id: 1, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", canChange: false}
-  ], 
+const temp = localStorage.getItem("data");
+const getSaveData = () => {
+  const temp = localStorage.getItem("data");
+  const initialValue = temp ? JSON.parse(temp) : { items: [{id: 1, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", canChange: false}], };
+  return initialValue;
 }
+
+const initialState = getSaveData();
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -14,7 +17,6 @@ function reducer(state = initialState, action) {
     case "updateCanChange": 
       return { ...state, items: action.payload };
     case "updateItem": 
-      console.log(action.payload)
       return { ...state, items: action.payload };
     default:
       return state;
@@ -34,7 +36,6 @@ const App = () => {
 
   const changeItem = (e) => {
     setValue(e.target.value);
-    console.log(value)
   }
 
   const dispatchNewItem = (e) => {
@@ -54,8 +55,6 @@ const App = () => {
   }
 
   const dispatchSaveItem = (id, item) => {
-    console.log(id);
-    console.log(item);
     const newItemList = data.items.map((item) => {
       const newItem = { ...item };
       if (item.id === id) {
@@ -88,7 +87,9 @@ const App = () => {
     });
   }
 
-  console.log(data.items)
+  useEffect(() => {
+    window.localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
 
   return (
     <div>
